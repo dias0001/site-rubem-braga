eb1ad9840f87c22458ae60460c55b31058bd1dc0
Luiz Gustavo Chaves dias <111753842+dias0001@users.noreply.github.com>
2022-08-30 11:19:59 -0300

    aula js 2

--- js/code.js
+++ js/code.js
@@ -3,19 +3,19 @@
 //cria um novo objeto da classe date
 const tempo = new Date()
 
-const txtData = tempo.getFullYear() + ' - hora ' + tempo.getHours() +  ' minuto: ' + tempo.getMinutes()
+const txtData = tempo.getFullYear() + ' - hora ' + tempo.getHours() + ' minuto: ' + tempo.getMinutes()
 
 //elementos DOM 
 const spanData = document.getElementById('data')
 const btfechar = document.getElementById('btfechar')
+const divModal = document.getElementById('divModal')
 //insere txtData dentro do ID "data
 spanData.innerText = txtData
 
 //ções do usuários (eventos)
 
-
 //Ao clicar no Id "btfechar"
-btfechar.addEventListener('click', function(){alert('olá mundo!!')})
-   //ocultar o modal - css display:none
-
-
+btfechar.addEventListener('click', function () {
+    //ocultar o divModal - css display:none por javascript
+    divModal = document.getElementById('divModal').style.disp
+})
