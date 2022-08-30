f398064473111d852cae7839df914a7f9cbd9f05
Luiz Gustavo Chaves dias <111753842+dias0001@users.noreply.github.com>
2022-08-30 10:54:23 -0300

    js-dia1

--- js/code.js
+++ js/code.js
@@ -1,2 +1,21 @@
 //Códigos para o hot site Rubens Braga
 
+//cria um novo objeto da classe date
+const tempo = new Date()
+
+const txtData = tempo.getFullYear() + ' - hora ' + tempo.getHours() +  ' minuto: ' + tempo.getMinutes()
+
+//elementos DOM 
+const spanData = document.getElementById('data')
+const btfechar = document.getElementById('btfechar')
+//insere txtData dentro do ID "data
+spanData.innerText = txtData
+
+//ções do usuários (eventos)
+
+
+//Ao clicar no Id "btfechar"
+btfechar.addEventListener('click', function(){alert('olá mundo!!')})
+   //ocultar o modal - css display:none
+
+
